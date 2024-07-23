455db5a0c069ead5ae095c0e5eae9b9735ae9770
Mark McClatchy <markmcclatchy@gmail.com>
2024-07-23 17:10:44 -0400

    [Sprint 1][Task 8] Update signup route

--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -1,5 +1,15 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{http::StatusCode, response::IntoResponse, Json};
+use serde::Deserialize;
 
-pub async fn post() -> impl IntoResponse {
+pub async fn post(Json(request): Json<SignupRequest>) -> impl IntoResponse {
+    println!("{:?}", request);
     StatusCode::OK.into_response()
+}
+
+#[derive(Deserialize, Debug)]
+pub struct SignupRequest {
+    pub email: String,
+    pub password: String,
+    #[serde(rename = "requires2FA")]
+    pub requires_2fa: bool,
 }
