eb59d9c24c9a6809b86c899cda53e12a9a4fb65b
Mark McClatchy <markmcclatchy@gmail.com>
2024-07-29 16:12:18 -0400

    [Sprint 2][Task 2] Add AuthAPIError

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,6 +1,8 @@
 use std::{error::Error, sync::Arc};
 
-use axum::{routing::post, serve::Serve, Router};
+use axum::{http::StatusCode, response::IntoResponse, routing::post, serve::Serve, Json, Router};
+use domain::error::AuthAPIError;
+use serde::{Deserialize, Serialize};
 use services::app_state::AppState;
 use tower_http::services::ServeDir;
 
@@ -33,5 +35,26 @@ impl Application {
     pub async fn run(self) -> Result<(), std::io::Error> {
         println!("listening on {}", &self.address);
         self.server.await
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ErrorResponse {
+    pub error: String,
+}
+
+impl IntoResponse for AuthAPIError {
+    fn into_response(self) -> axum::response::Response {
+        let (status, error_message) = match self {
+            AuthAPIError::UserAlreadyExists => (StatusCode::CONFLICT, "User already exists"),
+            AuthAPIError::InvalidCredentials => (StatusCode::BAD_REQUEST, "Invalid credentials"),
+            AuthAPIError::UnexpectedError => {
+                (StatusCode::INTERNAL_SERVER_ERROR, "Unexpected Error")
+            }
+        };
+        let body = Json(ErrorResponse {
+            error: error_message.to_string(),
+        });
+        (status, body).into_response()
     }
 }
