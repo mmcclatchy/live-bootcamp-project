52847e413c7084d67558ee1c838321c77013bbd6
Mark McClatchy <markmcclatchy@gmail.com>
2024-07-29 16:37:55 -0400

    [Sprint 2][Task 2] Update signup route

--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -3,23 +3,36 @@ use std::sync::Arc;
 use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
 
-use crate::{domain::user::User, services::app_state::AppState};
+use crate::{
+    domain::{error::AuthAPIError, user::User},
+    services::{app_state::AppState, hashmap_user_store::UserStoreError},
+};
 
 pub async fn post(
     State(state): State<Arc<AppState>>,
     Json(request): Json<SignupRequest>,
 ) -> impl IntoResponse {
+    let email = request.email;
+    let password = request.password;
+    if email.is_empty() || !email.contains('@') || password.len() < 8 {
+        return Err(AuthAPIError::InvalidCredentials);
+    }
+
     let user = User {
-        email: request.email,
-        password: request.password,
+        email,
+        password,
         requires_2fa: request.requires_2fa,
     };
     let mut user_store = state.user_store.write().await;
-    user_store.add_user(user).unwrap();
-    let response = Json(SignupResponse {
+    user_store.add_user(user).map_err(|e| match e {
+        UserStoreError::UserAlreadyExists => AuthAPIError::UserAlreadyExists,
+        _ => AuthAPIError::UnexpectedError,
+    })?;
+
+    let response = SignupResponse {
         message: "User created successfully".to_string(),
-    });
-    (StatusCode::CREATED, response)
+    };
+    Ok((StatusCode::CREATED, Json(response)))
 }
 
 #[derive(Deserialize, Debug)]
