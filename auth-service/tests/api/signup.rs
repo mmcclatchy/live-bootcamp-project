45c5c93cb2b46f3f5ae4d3852a3a99acc5374d0e
Mark McClatchy <markmcclatchy@gmail.com>
2024-07-29 16:03:59 -0400

    [Sprint 2][Task 2] Add new integration tests for signup errors

--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -47,3 +47,54 @@ async fn signup_should_return_201_if_valid_input() {
         signup_request
     );
 }
+
+#[tokio::test]
+async fn should_return_400_if_invalid_input() {
+    let app = TestApp::new().await;
+    let test_cases = [
+        serde_json::json!({
+            "email": "",
+            "password": "password123",
+            "requires2FA": true,
+        }),
+        serde_json::json!({
+            "email": "random_email",
+            "password": "password123",
+            "requires2FA": true,
+        }),
+        serde_json::json!({
+            "email": "test@email.com",
+            "password": "invalid",
+            "requires2FA": true,
+        }),
+    ];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(test_case).await;
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
+}
+
+#[tokio::test]
+async fn should_return_409_if_email_already_exists() {
+    let app = TestApp::new().await;
+    let random_email = get_random_email();
+    let signup_request = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+        "requires2FA": true,
+    });
+    app.post_signup(&signup_request).await;
+    let response = app.post_signup(&signup_request).await;
+    assert_eq!(
+        response.status().as_u16(),
+        409,
+        "Failed for input: {:?}",
+        signup_request
+    );
+}
