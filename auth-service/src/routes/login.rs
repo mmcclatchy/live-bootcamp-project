068ae1337dd0be2874a1db29f55ff92e0982c1aa
Mark McClatchy <markmcclatchy@gmail.com>
2024-08-09 01:20:30 -0400

    [Sprint 3][Task 1] Update login route

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,14 +1,40 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use std::sync::Arc;
 
-pub async fn post() -> impl IntoResponse {
-    StatusCode::OK.into_response()
-}
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::{extract::State, Json};
+use log::info;
+use serde::{Deserialize, Serialize};
 
+use crate::domain::{
+    data_stores::UserStore, email::Email, error::AuthAPIError, password::Password,
+};
+use crate::services::app_state::AppState;
+
+#[derive(Deserialize, Debug)]
 pub struct LoginRequest {
-    pub email: String,
-    pub password: String,
+    email: String,
+    password: String,
 }
 
-pub struct LoginResponse {
-    pub message: String,
+#[derive(Serialize)]
+pub struct SignupResponse {
+    message: String,
+}
+
+pub async fn post<T: UserStore>(
+    State(state): State<Arc<AppState<T>>>,
+    Json(payload): Json<LoginRequest>,
+) -> Result<impl IntoResponse, AuthAPIError> {
+    info!("[REST][POST][/signup] Received request: {:?}", payload);
+
+    let email = Email::parse(payload.email).map_err(AuthAPIError::InvalidEmail)?;
+    let password = Password::parse(payload.password).map_err(AuthAPIError::InvalidPassword)?;
+
+    let user_store = state.user_store.write().await;
+    if user_store.validate_user(&email, &password).await.is_err() {
+        return Err(AuthAPIError::InvalidCredentials);
+    }
+
+    Ok(StatusCode::OK.into_response())
 }
