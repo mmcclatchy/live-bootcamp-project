f098b3394220968378a7ddf0d26021afa288055c
Mark McClatchy <markmcclatchy@gmail.com>
2024-08-09 16:09:55 -0400

    [Sprint 3][Task 2] Update login route with cookies

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::{extract::State, Json};
+use axum_extra::extract::CookieJar;
 use log::info;
 use serde::Deserialize;
 
@@ -10,6 +11,7 @@ use crate::domain::{
     data_stores::UserStore, email::Email, error::AuthAPIError, password::Password,
 };
 use crate::services::app_state::AppState;
+use crate::utils::auth::generate_auth_cookie;
 
 #[derive(Deserialize, Debug)]
 pub struct LoginRequest {
@@ -19,17 +21,31 @@ pub struct LoginRequest {
 
 pub async fn post<T: UserStore>(
     State(state): State<Arc<AppState<T>>>,
+    jar: CookieJar,
     Json(payload): Json<LoginRequest>,
-) -> Result<impl IntoResponse, AuthAPIError> {
+) -> (CookieJar, Result<impl IntoResponse, AuthAPIError>) {
     info!("[REST][POST][/signup] Received request: {:?}", payload);
 
-    let email = Email::parse(payload.email).map_err(AuthAPIError::InvalidEmail)?;
-    let password = Password::parse(payload.password).map_err(AuthAPIError::InvalidPassword)?;
+    let email = match Email::parse(payload.email) {
+        Ok(email) => email,
+        Err(msg) => return (jar, Err(AuthAPIError::InvalidEmail(msg))),
+    };
+
+    let password = match Password::parse(payload.password) {
+        Ok(password) => password,
+        Err(msg) => return (jar, Err(AuthAPIError::InvalidPassword(msg))),
+    };
 
     let user_store = state.user_store.write().await;
     if user_store.validate_user(&email, &password).await.is_err() {
-        return Err(AuthAPIError::InvalidCredentials);
-    }
+        return (jar, Err(AuthAPIError::InvalidCredentials));
+    };
 
-    Ok(StatusCode::OK.into_response())
+    let auth_cookie = match generate_auth_cookie(&email) {
+        Ok(cookie) => cookie,
+        Err(_) => return (jar, Err(AuthAPIError::UnexpectedError)),
+    };
+
+    let updated_jar = jar.add(auth_cookie);
+    (updated_jar, Ok(StatusCode::OK.into_response()))
 }
