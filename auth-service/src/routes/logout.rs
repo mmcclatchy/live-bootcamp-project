ebe3b25c24ffe70829dff5b965a0841dc330e99e
Mark McClatchy <markmcclatchy@gmail.com>
2024-08-10 17:55:06 -0400

    [Sprint 3][Task 3] Add 200 and 400 test cases for logout route

--- auth-service/src/routes/logout.rs
+++ auth-service/src/routes/logout.rs
@@ -1,5 +1,5 @@
 use axum::{http::StatusCode, response::IntoResponse};
-use axum_extra::extract::{cookie, CookieJar};
+use axum_extra::extract::CookieJar;
 
 use crate::{
     domain::error::AuthAPIError,
@@ -15,6 +15,5 @@ pub async fn post(jar: CookieJar) -> Result<(CookieJar, impl IntoResponse), Auth
     validate_token(&token)
         .await
         .map_err(|_| AuthAPIError::InvalidToken)?;
-    let jar = jar.remove(cookie::Cookie::from(JWT_COOKIE_NAME));
     Ok((jar, StatusCode::OK))
 }
--- auth-service/tests/api/rest_logout.rs
+++ auth-service/tests/api/rest_logout.rs
@@ -1,7 +1,63 @@
-use auth_service::{api::rest::ErrorResponse, utils::constants::JWT_COOKIE_NAME};
+use auth_service::{
+    api::rest::ErrorResponse,
+    utils::constants::{JWT_COOKIE_NAME, JWT_SECRET},
+};
 use reqwest::Url;
+use serde_json::json;
 
 use crate::helpers::RESTTestApp;
+
+async fn create_app_with_logged_in_cookie() -> RESTTestApp {
+    let app = RESTTestApp::new().await;
+    let signup_body = json!({
+        "email": "test@example.com",
+        "password": "P@ssw0rd",
+        "requires2FA": false,
+    });
+    let signup_response = app.post_signup(&signup_body).await;
+    assert_eq!(signup_response.status(), 201);
+    let login_body = json!({
+        "email": "test@example.com",
+        "password": "P@ssw0rd",
+    });
+    let login_response = app.post_login(&login_body).await;
+    assert_eq!(login_response.status(), 200);
+    let cookie = login_response
+        .cookies()
+        .find(|c| c.name() == JWT_COOKIE_NAME)
+        .expect("[ERROR][Test Helper][create_app_with_logged_in_cookie] No auth cookie returned");
+    assert!(!cookie.value().is_empty());
+    app
+}
+
+#[tokio::test]
+async fn should_return_200_if_valid_jwt_cookie() {
+    let app = create_app_with_logged_in_cookie().await;
+    let logout_response = app.post_logout().await;
+    assert_eq!(logout_response.status(), 200);
+    let cookie = logout_response
+        .cookies()
+        .find(|c| c.name() == JWT_COOKIE_NAME)
+        .expect("[ERROR][TEST][should_return_200_if_valid_jwt_cookie] No cookie returned");
+    assert!(cookie.value().is_empty());
+}
+
+#[tokio::test]
+async fn should_return_400_if_logout_called_twice_in_a_row() {
+    let app = create_app_with_logged_in_cookie().await;
+    let logout_response = app.post_logout().await;
+    assert_eq!(logout_response.status(), 200);
+    let logout_response = app.post_logout().await;
+    assert_eq!(logout_response.status(), 400);
+    assert_eq!(
+        logout_response
+            .json::<ErrorResponse>()
+            .await
+            .expect("[ERROR][TEST][should_return_400_if_jwt_cookie_missing] Could not deserialize response body to ErrorResponse")
+            .error,
+        "Missing auth token".to_owned()
+    );
+}
 
 #[tokio::test]
 async fn should_return_400_if_jwt_cookie_missing() {
@@ -26,12 +82,9 @@ async fn should_return_400_if_jwt_cookie_missing() {
 async fn should_return_401_if_invalid_token() {
     let app = RESTTestApp::new().await;
     app.cookie_jar.add_cookie_str(
-        &format!(
-            "{}=invalid; HttpOnly; SameSite=Lax; Secure; Path=/",
-            JWT_COOKIE_NAME,
-        ),
+        &format!("{JWT_COOKIE_NAME}=invalid; HttpOnly; SameSite=Lax; Secure; Path=/"),
         &Url::parse("http://127.0.0.1")
-            .expect("[ERROR][TEST][should_return_401_if_invalid_token] Failed to parse URL"),
+            .expect("[ERROR][TEST][create_app_with_cookie] Failed to parse URL"),
     );
     let logout_response = app.post_logout().await;
     assert_eq!(logout_response.status(), 401);
