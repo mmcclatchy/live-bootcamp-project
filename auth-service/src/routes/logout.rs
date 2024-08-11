4a59aa6f6af861d0d70bf22dbc3a1bc9faddf54f
Mark McClatchy <markmcclatchy@gmail.com>
2024-08-11 13:03:54 -0400

    [Sprint 3][Task 5] Update logout route for 200 test case

--- auth-service/src/routes/logout.rs
+++ auth-service/src/routes/logout.rs
@@ -1,20 +1,36 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use std::sync::Arc;
+
+use axum::{extract::State, http::StatusCode, response::IntoResponse};
 use axum_extra::extract::{cookie, CookieJar};
 
-use crate::{
-    domain::error::AuthAPIError,
-    utils::{auth::validate_token, constants::JWT_COOKIE_NAME},
+use crate::domain::{
+    data_stores::{BannedTokenStore, UserStore},
+    error::AuthAPIError,
 };
+use crate::services::app_state::AppState;
+use crate::utils::{auth::validate_token, constants::JWT_COOKIE_NAME};
 
-pub async fn post(jar: CookieJar) -> Result<(CookieJar, impl IntoResponse), AuthAPIError> {
+pub async fn post<T: BannedTokenStore, U: UserStore>(
+    State(state): State<Arc<AppState<T, U>>>,
+    jar: CookieJar,
+) -> Result<(CookieJar, impl IntoResponse), AuthAPIError> {
     let cookie = match jar.get(JWT_COOKIE_NAME) {
         Some(cookie) => cookie,
         None => return Err(AuthAPIError::MissingToken),
     };
+
     let token = cookie.value().to_owned();
     validate_token(&token)
         .await
         .map_err(|_| AuthAPIError::InvalidToken)?;
+
     let jar = jar.remove(cookie::Cookie::from(JWT_COOKIE_NAME));
+
+    let mut banned_token_store = state.banned_token_store.write().await;
+    banned_token_store
+        .add_token(token)
+        .await
+        .map_err(|_| AuthAPIError::UnexpectedError)?;
+
     Ok((jar, StatusCode::OK))
 }
