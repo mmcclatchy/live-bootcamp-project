9bb07d30542df2704561763be6a763242e643645
Mark McClatchy <markmcclatchy@gmail.com>
2024-08-10 22:01:51 -0400

    [Sprint 3][Task 4] Update verify-token route for 422 test case

--- auth-service/src/routes/verify_token.rs
+++ auth-service/src/routes/verify_token.rs
@@ -1,5 +1,11 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{http::StatusCode, response::IntoResponse, Json};
+use serde::Deserialize;
 
-pub async fn post() -> impl IntoResponse {
+#[derive(Debug, Deserialize)]
+pub struct VerifyTokenRequest {
+    _token: String,
+}
+
+pub async fn post(Json(_token): Json<VerifyTokenRequest>) -> impl IntoResponse {
     StatusCode::OK.into_response()
 }
